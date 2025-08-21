4395b40b1d63f00ed41cb1d84c2cf4b9ad3dc7eb
huangguoliang <huangguoliang@renwohua.com>
2025-08-21 21:54:59 +0800

    new

--- run/run_t2v.bat
+++ run/run_t2v.bat
@@ -18,5 +18,5 @@ SET ARGS=%WAN2_1_T2V_ARGS%
 :: 删除环境变量
 setx WAN2_1_T2V_ARGS ""
 echo ARGS: %ARGS%
-REM python D:\public\AI\Wan2.1\generate.py --task t2v-1.3B --size "832*480" --ckpt_dir ./Wan2.1-T2V-1.3B --offload_model True --t5_cpu --sample_shift 8 --sample_guide_scale 6 --prompt "Two anthropomorphic cats in comfy boxing gear and bright gloves fight intensely on a spotlighted stage."
+python D:\public\AI\Wan2.1\generate.py --task t2v-1.3B --size "832*480" --ckpt_dir ./Wan2.1-T2V-1.3B --offload_model True --t5_cpu --sample_shift 8 --sample_guide_scale 6 --prompt "Two anthropomorphic cats in comfy boxing gear and bright gloves fight intensely on a spotlighted stage."
 pause
