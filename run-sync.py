05d34945fd125f81fe72be1f387f1796938c246f
huangguoliang <huangguoliang@renwohua.com>
2025-08-31 00:17:39 +0800

    refactor(run-sync.py): 注释掉自动保存文件名的代码块
    将负责生成自动保存文件名的代码块添加了注释，以便于后续的代码阅读和维护。

--- run-sync.py
+++ run-sync.py
@@ -79,12 +79,12 @@ def main():
                         seed=args.base_seed,
                         offload_model=args.offload_model)
 
-                    if args.save_file is None:
-                        formatted_time = datetime.now().strftime("%Y%m%d_%H%M%S")
-                        formatted_prompt = prompt.replace(" ", "_").replace("/",
-                                                                                 "_")[:50]
-                        suffix = '.png' if "t2i" in args.task else '.mp4'
-                        args.save_file = f"{args.task}_{args.size.replace('*','x') if sys.platform=='win32' else args.size}_{args.ulysses_size}_{args.ring_size}_{formatted_prompt}_{formatted_time}" + suffix
+                    # if args.save_file is None:
+                    formatted_time = datetime.now().strftime("%Y%m%d_%H%M%S")
+                    formatted_prompt = prompt.replace(" ", "_").replace("/",
+                                                                             "_")[:50]
+                    suffix = '.png' if "t2i" in args.task else '.mp4'
+                    args.save_file = f"{args.task}_{args.size.replace('*','x') if sys.platform=='win32' else args.size}_{args.ulysses_size}_{args.ring_size}_{formatted_prompt}_{formatted_time}" + suffix
 
                     if "t2i" in args.task:
                         printMy(f"Saving generated image to {args.save_file}")
